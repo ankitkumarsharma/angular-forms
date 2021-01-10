6366cbb80028115bd4501db3b2972e99a5c7f61c
Ankit Kumar Sharma <aksharma.dixit@gmail.com>
2021-01-10 23:59:23 +0530

    add validation

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormType } from '../core/app.types';
 
 @Component({
@@ -10,18 +10,24 @@ import { ReactiveFormType } from '../core/app.types';
 export class ReactiveFormsComponent implements OnInit {
   regForm!: FormGroup;
   getData!: ReactiveFormType;
+  submitted = false;
   constructor(private _fb:FormBuilder) { }
 
   ngOnInit(): void {
     this.regForm = this._fb.group({
-      firstName: [''],
+      firstName: ['', Validators.required],
       lastName: [''],
-      email: [''],
+      email: ['', [Validators.required, Validators.email]],
       address: ['']
     });
   }
   onSubmit(value:ReactiveFormType){
-    this.getData = value;
+    this.submitted = true;
+    if(this.regForm.valid){
+      this.getData = value;
+    } else {
+      this.regForm.markAllAsTouched();
+    }
   }
 
 }
